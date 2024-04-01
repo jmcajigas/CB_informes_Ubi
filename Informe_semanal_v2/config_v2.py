7c1e91aafd69cbb7d453bd61dcba253dde9b3170
Juan Ocampo <juan_pablo.ocampo@uao.edu.co>
2024-04-01 16:09:59 -0500

    Informe del 18 al 24 marzo 2024
    Informe del 18 al 24 marzo 2024 - ok

--- Informe_semanal_v2/config_v2.py
+++ Informe_semanal_v2/config_v2.py
@@ -5,8 +5,8 @@ NIGHT_HOURS = [0, 1, 2, 3, 4, 5, 19, 20, 21, 22, 23]
 # date as start of study. Basically all
 # dates must be mondays.
 BASELINE = ['2023-09-15', '2024-03-18']
-STUDY = ['2024-03-11', '2024-03-18']
-PAST_WEEK = ['2024-03-04', '2024-03-11']
+STUDY = ['2024-03-18', '2024-03-25']
+PAST_WEEK = ['2024-03-11', '2024-03-18']
 
 DATE_INTERVALS_TO_DISCARD = {
 }
