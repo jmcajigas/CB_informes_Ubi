49dca164a978dd36ee392a2d18e931e3db6a8a06
Juan Ocampo <juan_pablo.ocampo@uao.edu.co>
2024-04-08 14:36:55 -0500

    Informe semana 1 al 7 abril 2024
     Informe semana 1 al 7 abril 2024  ok

--- Informe_semanal_v2/query.ps1
+++ Informe_semanal_v2/query.ps1
@@ -1,11 +1,17 @@
-# Ruta al script activate del entorno Conda
-$activateScript = "C:/ProgramData/Anaconda3/Scripts/activate"
+# Activo mi entorno base y mi env
+& C:\ProgramData\Anaconda3\shell\condabin\conda-hook.ps1; conda activate base
+& C:\ProgramData\Anaconda3\shell\condabin\conda-hook.ps1; conda activate nuevo2-env
+# 2. Correr tools/request.ipynb para hacer el request a Ubidots.
+& python Informe_semanal_v2\tools\request.py
+#& C:\ProgramData\Anaconda3\python.exe Informe_semanal_v2\tools\request.py
+# 4. Borrar los notebooks individuales por sede (main/notebooks/individual)
+& Remove-Item 'Informe_semanal_v2\main\notebooks\individual\' -Recurse
 
-# Ejecuta el script activate para activar el entorno
-& $activateScript nuevo2-env
+#5. Replicar el modelo para cada sede usando tools/builder.ipynb
+& python Informe_semanal_v2\tools\builder.py
 
-# Ruta al archivo .ipynb que quieres ejecutar
-$notebookPath = "C:\Users\jpocampo\Desktop\Informe_Bancolombia\CB_informes_Ubi\Informe_semanal_v2\tools\request.ipynb"
+#7. Borrar la carpeta main/\_build (o manipular la configuración de ejecución entre cache y force. Borrar funciona como hacer force)#
+& Remove-Item 'Informe_semanal_v2\main\_build' -Recurse
+# 10. Correr el comando: "jb build main" para compilar los notebooks
+& jb build "Informe_semanal_v2\main"
 
-# Ejecuta el archivo .ipynb con jupyter nbconvert
-jupyter nbconvert --to notebook --execute $notebookPath
