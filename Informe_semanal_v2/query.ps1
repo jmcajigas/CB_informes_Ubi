a8a6eca4e898b5c13395244b48098686ab24061a
Juan Ocampo <juan_pablo.ocampo@uao.edu.co>
2024-04-15 16:01:43 -0500

    Informe del 8al 14 abril 2024
    Informe del 8al 14 abril 2024

--- Informe_semanal_v2/query.ps1
+++ Informe_semanal_v2/query.ps1
@@ -2,10 +2,10 @@
 & C:\ProgramData\Anaconda3\shell\condabin\conda-hook.ps1; conda activate base
 & C:\ProgramData\Anaconda3\shell\condabin\conda-hook.ps1; conda activate nuevo2-env
 # 2. Correr tools/request.ipynb para hacer el request a Ubidots.
-& python Informe_semanal_v2\tools\request.py
-#& C:\ProgramData\Anaconda3\python.exe Informe_semanal_v2\tools\request.py
+##& python Informe_semanal_v2\tools\request.py
 # 4. Borrar los notebooks individuales por sede (main/notebooks/individual)
-& Remove-Item 'Informe_semanal_v2\main\notebooks\individual\' -Recurse
+& Remove-Item 'Informe_semanal_v2\main\notebooks\individual\*' -Recurse -Force
+
 
 #5. Replicar el modelo para cada sede usando tools/builder.ipynb
 & python Informe_semanal_v2\tools\builder.py
