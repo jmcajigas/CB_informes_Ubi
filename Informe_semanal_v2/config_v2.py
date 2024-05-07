b0fe4875e7d3a137afcb5a6389493fe22eec873c
Juan Ocampo <juan_pablo.ocampo@uao.edu.co>
2024-05-07 16:52:09 -0500

    Informe del 29 abril al 5 mayo 2024
    ok

--- Informe_semanal_v2/config_v2.py
+++ Informe_semanal_v2/config_v2.py
@@ -4,9 +4,9 @@ NIGHT_HOURS = [0, 1, 2, 3, 4, 5, 19, 20, 21, 22, 23]
 # last baseline date must be the same
 # date as start of study. Basically all
 # dates must be mondays.
-BASELINE = ['2023-09-15', '2024-04-22']
-STUDY = ['2024-04-22', '2024-04-29']
-PAST_WEEK = ['2024-04-15', '2024-04-22']
+BASELINE = ['2024-01-01', '2024-04-29']
+STUDY = ['2024-04-29', '2024-05-06']
+PAST_WEEK = ['2024-04-22', '2024-04-29']
 
 DATE_INTERVALS_TO_DISCARD = {
 }
--- Informe_semanal_v2/query.ps1
+++ Informe_semanal_v2/query.ps1
@@ -4,14 +4,14 @@
 # 2. Correr tools/request.ipynb para hacer el request a Ubidots.
 & python Informe_semanal_v2\tools\request.py
 # 4. Borrar los notebooks individuales por sede (main/notebooks/individual)
-& Remove-Item 'Informe_semanal_v2\main\notebooks\individual\*' -Recurse -Force
+& Remove-Item 'C:\Proyectos Digitalización\Bancolombia\CB_informes_Ubi\Informe_semanal_v2\main\notebooks\individual\*' -Recurse -Force
 
 
 #5. Replicar el modelo para cada sede usando tools/builder.ipynb
-& python Informe_semanal_v2\tools\builder.py
+& python tools\builder.py
 
 #7. Borrar la carpeta main/\_build (o manipular la configuración de ejecución entre cache y force. Borrar funciona como hacer force)#
-& Remove-Item 'Informe_semanal_v2\main\_build' -Recurse
+& Remove-Item 'main\_build' -Recurse
 # 10. Correr el comando: "jb build main" para compilar los notebooks
-& jb build "Informe_semanal_v2\main"
+& jb build "main"
 
