4569a811ceb851ece12b0704f9bc2894d9808fb5
Juan Ocampo <juan_pablo.ocampo@uao.edu.co>
2024-07-24 09:38:50 -0500

    Informe del 15 al 21 julio 2024

--- Informe_semanal_v2/config_v2.py
+++ Informe_semanal_v2/config_v2.py
@@ -4,9 +4,9 @@ NIGHT_HOURS = [0, 1, 2, 3, 4, 5, 19, 20, 21, 22, 23]
 # last baseline date must be the same
 # date as start of study. Basically all
 # dates must be mondays.
-BASELINE = ['2024-01-01', '2024-07-08']
-STUDY = ['2024-07-08', '2024-07-15']
-PAST_WEEK = ['2024-07-01', '2024-07-08']
+BASELINE = ['2024-04-01', '2024-07-15']
+STUDY = ['2024-07-15', '2024-07-22']
+PAST_WEEK = ['2024-07-08', '2024-07-15']
 
 DATE_INTERVALS_TO_DISCARD = {
 }
