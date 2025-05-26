09e516a8f8a958bfd4a0fb661acabc48ab031f46
jmcajigas <115262308+jmcajigas@users.noreply.github.com>
2025-05-26 10:52:53 -0500

    Informe del 19 al 25 mayo 2025
    oka

--- Informe_semanal_v2/config_v2.py
+++ Informe_semanal_v2/config_v2.py
@@ -4,9 +4,9 @@ NIGHT_HOURS = [0, 1, 2, 3, 4, 5, 19, 20, 21, 22, 23]
 # last baseline date must be the same
 # date as start of study. Basically alls
 # dates must be mondays.
-BASELINE = ['2025-02-01', '2025-05-12']
-STUDY = ['2025-05-12', '2025-05-19']
-PAST_WEEK = ['2025-05-05', '2025-05-12']
+BASELINE = ['2025-02-01', '2025-05-19']
+STUDY = ['2025-05-19', '2025-05-26']
+PAST_WEEK = ['2025-05-12', '2025-05-19']
 
 DATE_INTERVALS_TO_DISCARD = {
 }
